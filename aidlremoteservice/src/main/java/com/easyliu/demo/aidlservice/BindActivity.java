2e421e881746f72f3c15d862bde63f6acb1be2ab
249674239@qq.com <249674239@qq.com>
2018-03-02 16:24:36 +0800

    mutual

---
+++ aidlremoteservice/src/main/java/com/easyliu/demo/aidlservice/BindActivity.java
@@ -0,0 +1,16 @@
+package com.easyliu.demo.aidlservice;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BindActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, RemoteService.class);
+        startService(intent);
+    }
+
+}
